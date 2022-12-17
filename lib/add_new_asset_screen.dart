21743381487f9bbe16132335f8f01f1fde4f4b1b
ishuley <ishuley@gmail.com>
2022-12-17 17:11:53 -0500

    refactored all state into parent widgets, then made all cross-wdiget interaction occur through function callbacks (after learning more about how Flutter works)

--- lib/add_new_asset_screen.dart
+++ lib/add_new_asset_screen.dart
@@ -2,38 +2,104 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'api_service.dart';
 
-const List<String> stockDataSourcesList = <String>[
-  'Manual Qty',
-  'Transfer Agent',
-  'Broker API',
-];
-const List<String> cryptoDataSourcesList = <String>[
-  'Blockchain Address',
-  'Exchange API',
-  'Manual Qty',
-];
-const List<String> nftDataSourcesList = <String>[
-  'Blockchain Address',
-  'Manual Qty',
-];
-const List<String> cashDataSourcesList = <String>[
-  'Manual Qty',
-];
-
-String _currentDataSource = stockDataSourcesList.first;
-List<String> dataSourceDropdownValuesList = stockDataSourcesList;
-String _assetType = "stock";
-String _currentAssetName = "GameStop";
-int _assetSelection = 0;
-
 class AddNewAssetScreen extends StatefulWidget {
   const AddNewAssetScreen({super.key});
+
+  static const List<String> stockDataSourcesList = <String>[
+    'Manual Qty',
+    'Transfer Agent',
+    'Broker API',
+  ];
+  static const List<String> cryptoDataSourcesList = <String>[
+    'Blockchain Address',
+    'Exchange API',
+    'Manual Qty',
+  ];
+  static const List<String> nftDataSourcesList = <String>[
+    'Blockchain Address',
+    'Manual Qty',
+  ];
+  static const List<String> cashDataSourcesList = <String>[
+    'Manual Qty',
+  ];
 
   @override
   State<AddNewAssetScreen> createState() => _AddNewAssetScreenState();
 }
 
 class _AddNewAssetScreenState extends State<AddNewAssetScreen> {
+  String currentDataSource = AddNewAssetScreen.stockDataSourcesList.first;
+  List<String> dataSourceDropdownValues =
+      AddNewAssetScreen.stockDataSourcesList;
+  String assetType = "stock";
+  String currentAssetName = "GameStop";
+  int assetSelection = 0;
+  String currentDataSourceLabel = "Enter quantity manually:";
+
+  List<String> dataSourcesListByAssetType() {
+    if (assetType == "crypto") {
+      return AddNewAssetScreen.cryptoDataSourcesList;
+    }
+    if (assetType == "nft") {
+      return AddNewAssetScreen.nftDataSourcesList;
+    }
+    if (assetType == "cash") {
+      return AddNewAssetScreen.cashDataSourcesList;
+    }
+    return AddNewAssetScreen.stockDataSourcesList;
+  }
+
+  assetTypeChanged(int currentAssetSelection) {
+    setState(() {
+      assetSelection = currentAssetSelection;
+      determineAssetTypeFromSelection(assetSelection);
+      dataSourceDropdownValues = dataSourcesListByAssetType();
+      currentDataSource = dataSourceDropdownValues.first;
+      // TODO make currentAssetName remember the last asset selected from a category after changing
+      currentAssetName = APIService(assetType).getAssetList().first;
+      dataSourceChanged(currentDataSource);
+    });
+  }
+
+  void determineAssetTypeFromSelection(int assetSelection) {
+    if (assetSelection == 0) {
+      assetType = "stock";
+    }
+    if (assetSelection == 1) {
+      assetType = "crypto";
+    }
+    if (assetSelection == 2) {
+      assetType = "nft";
+    }
+    if (assetSelection == 3) {
+      assetType = "cash";
+    }
+  }
+
+  dataSourceChanged(String dataSource) {
+    setState(() {
+      currentDataSource = dataSource;
+      currentDataSourceLabel = getDataSourceLabel();
+      //TODO implement broker and exchange API support, then check if the source is an API, then ask which supported exchange or broker they wish to use
+    });
+  }
+
+  assetDropdownChanged(String currentAssetName) {
+    setState(() {
+      this.currentAssetName = currentAssetName;
+    });
+  }
+
+  String getDataSourceLabel() {
+    if (currentDataSource.endsWith("API")) {
+      return "Enter Read-Only API Key: ";
+    }
+    if (currentDataSource.endsWith("Address")) {
+      return "Enter blockchain address: ";
+    }
+    return "Enter quantity manually: ";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,24 +111,40 @@ class _AddNewAssetScreenState extends State<AddNewAssetScreen> {
         ),
         body: Container(
             color: Colors.grey[850],
-            child: Column(children: const [
-              AssetTypeSelection(),
-              DataSourceDropdown(),
-              AssetDropdown(),
-              DataSourceLabel(),
-              AcceptCancelButton(),
+            child: Column(children: [
+              AssetTypeSelection(
+                  assetSelection: assetSelection,
+                  assetTypeChangedCallback: assetTypeChanged),
+              DataSourceDropdown(
+                  currentDataSource: currentDataSource,
+                  dataSourceDropdownValues: dataSourceDropdownValues,
+                  dataSourceChangedCallback: dataSourceChanged),
+              AssetDropdown(
+                  currentAssetName: currentAssetName,
+                  assetType: assetType,
+                  assetDropdownChangedCallback: assetDropdownChanged),
+              DataSourceLabel(
+                dataSourceLabel: currentDataSourceLabel,
+              ),
+              const AcceptCancelButton(),
             ])));
   }
 }
 
 class AssetTypeSelection extends StatefulWidget {
-  const AssetTypeSelection({super.key});
-
+  final int assetSelection;
+  final ValueChanged<int> assetTypeChangedCallback;
+  const AssetTypeSelection(
+      {super.key,
+      required this.assetSelection,
+      required this.assetTypeChangedCallback});
   @override
   State<AssetTypeSelection> createState() => _AssetTypeSelectionState();
 }
 
 class _AssetTypeSelectionState extends State<AssetTypeSelection> {
+  int _assetSelection = 0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -71,29 +153,10 @@ class _AssetTypeSelectionState extends State<AssetTypeSelection> {
           child: CupertinoSlidingSegmentedControl(
         groupValue: _assetSelection,
         onValueChanged: (int? choice) {
+          widget.assetTypeChangedCallback(choice!);
+
           setState(() {
-            _assetSelection = choice!;
-            if (_assetSelection == 0) {
-              dataSourceDropdownValuesList = stockDataSourcesList;
-              _currentDataSource = stockDataSourcesList.first;
-              _assetType = "stock";
-            }
-            if (_assetSelection == 1) {
-              dataSourceDropdownValuesList = cryptoDataSourcesList;
-              _currentDataSource = cryptoDataSourcesList.first;
-              _assetType = "crypto";
-            }
-            if (_assetSelection == 2) {
-              dataSourceDropdownValuesList = nftDataSourcesList;
-              _currentDataSource = nftDataSourcesList.first;
-              _assetType = "nft";
-            }
-            if (_assetSelection == 3) {
-              dataSourceDropdownValuesList = cashDataSourcesList;
-              _currentDataSource = cashDataSourcesList.first;
-              _assetType = "cash";
-            }
-            _currentAssetName = APIService(_assetType).getAssetList().first;
+            _assetSelection = choice;
           });
         },
         backgroundColor: Colors.black38,
@@ -109,27 +172,27 @@ class _AssetTypeSelectionState extends State<AssetTypeSelection> {
   }
 }
 
-class DataSourceDropdown extends StatefulWidget {
-  const DataSourceDropdown({super.key});
+class DataSourceDropdown extends StatelessWidget {
+  final String currentDataSource;
+  final List<String> dataSourceDropdownValues;
+  final ValueChanged<String> dataSourceChangedCallback;
+  const DataSourceDropdown(
+      {super.key,
+      required this.currentDataSource,
+      required this.dataSourceDropdownValues,
+      required this.dataSourceChangedCallback});
 
-  @override
-  State<DataSourceDropdown> createState() => _DataSourceDropdownState();
-}
-
-class _DataSourceDropdownState extends State<DataSourceDropdown> {
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(8.0),
         child: DropdownButton<String>(
           focusColor: Colors.black,
-          onChanged: ((String? newValue) {
-            setState(() {
-              _currentDataSource = newValue!;
-            });
+          onChanged: ((String? selectedDataSource) {
+            dataSourceChangedCallback(selectedDataSource!);
           }),
-          value: _currentDataSource,
-          items: dataSourceDropdownValuesList
+          value: currentDataSource,
+          items: dataSourceDropdownValues
               .map<DropdownMenuItem<String>>((String dataSourceName) {
             return DropdownMenuItem<String>(
               value: dataSourceName,
@@ -143,27 +206,29 @@ class _DataSourceDropdownState extends State<DataSourceDropdown> {
   }
 }
 
-class AssetDropdown extends StatefulWidget {
-  const AssetDropdown({super.key});
+class AssetDropdown extends StatelessWidget {
+  final String assetType;
+  final String currentAssetName;
+  final ValueChanged<String> assetDropdownChangedCallback;
 
-  @override
-  State<AssetDropdown> createState() => _AssetDropdownState();
-}
+  const AssetDropdown({
+    super.key,
+    required this.assetType,
+    required this.currentAssetName,
+    required this.assetDropdownChangedCallback,
+  });
 
-class _AssetDropdownState extends State<AssetDropdown> {
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(8.0),
         child: DropdownButton<String>(
           focusColor: Colors.black,
-          onChanged: ((String? newValue) {
-            setState(() {
-              _currentAssetName = newValue!;
-            });
+          onChanged: ((String? chosenAssetName) {
+            assetDropdownChangedCallback(chosenAssetName!);
           }),
-          value: _currentAssetName,
-          items: APIService(_assetType)
+          value: currentAssetName,
+          items: APIService(assetType)
               .getAssetList()
               .map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
@@ -178,42 +243,19 @@ class _AssetDropdownState extends State<AssetDropdown> {
   }
 }
 
-class DataSourceLabel extends StatefulWidget {
-  const DataSourceLabel({super.key});
+class DataSourceLabel extends StatelessWidget {
+  const DataSourceLabel({super.key, required this.dataSourceLabel});
 
-  @override
-  State<DataSourceLabel> createState() => _DataSourceLabelState();
-}
+  final String dataSourceLabel;
 
-class _DataSourceLabelState extends State<DataSourceLabel> {
   @override
   Widget build(BuildContext context) {
-    if (_currentDataSource.endsWith("API")) {
-      return const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.bottomLeft,
-          child: Text("Read-only API Key: ",
-              style: TextStyle(color: Colors.white)),
-        ),
-      );
-    }
-    if (_currentDataSource.endsWith("Address")) {
-      return const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.bottomLeft,
-          child: Text("Blockchain address: ",
-              style: TextStyle(color: Colors.white)),
-        ),
-      );
-    }
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Align(
         alignment: Alignment.bottomLeft,
-        child: Text("Enter quantity manually: ",
-            style: TextStyle(color: Colors.white)),
+        child:
+            Text(dataSourceLabel, style: const TextStyle(color: Colors.white)),
       ),
     );
   }
