bfc3b9133f08c33008293d033694c187323b891f
ishuley <ishuley@gmail.com>
2023-01-02 02:39:55 -0500

    minor formatting changes

--- lib/asset.dart
+++ lib/asset.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
 import 'api_service.dart';
 
 enum AssetType { stock, crypto, cash }
@@ -69,11 +68,8 @@ class Cash extends Asset {
 class AssetCard extends StatelessWidget {
   final Asset asset;
   final String vsTicker;
-
   final double price;
-
   double get totalValue => price * asset.quantity;
-
   final String marketCapString;
 
   const AssetCard({
@@ -87,76 +83,73 @@ class AssetCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: FractionallySizedBox(
-        widthFactor: 1,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      asset.ticker,
-                      textScaleFactor: 1.6,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      asset.name,
-                      textScaleFactor: 0.8,
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Qty: ",
-                    ),
-                    Text(asset.quantity.toString())
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Price ($vsTicker):",
-                    ),
-                    Text(price.toString()),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      textAlign: TextAlign.center,
-                      "Total ($vsTicker): ",
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      textAlign: TextAlign.center,
-                      totalValue.toString(),
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Text(
-                    marketCapString,
-                    textScaleFactor: 0.9,
+      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    asset.ticker,
+                    textScaleFactor: 1.6,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            )
-          ],
-        ),
+                  Text(
+                    asset.name,
+                    textScaleFactor: 0.8,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Qty: ",
+                  ),
+                  Text(asset.quantity.toStringAsFixed(2))
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Price (${vsTicker.toUpperCase()}):",
+                  ),
+                  Text(price.toStringAsFixed(2)),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    textAlign: TextAlign.center,
+                    "Total (${vsTicker.toUpperCase()}): ",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    textAlign: TextAlign.center,
+                    totalValue.toStringAsFixed(2),
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(
+                  marketCapString,
+                  textScaleFactor: 0.9,
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
